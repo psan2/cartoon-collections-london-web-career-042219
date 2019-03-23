a09a4c4a686057eac98c5f27102e1d549880480e
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-23 12:39:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(roster)
   roster.each.with_index(1) {|dwarf,line_place| puts "#{line_place}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect{|x| x + "!"}
 end
 
 def long_planeteer_calls# code an argument here
