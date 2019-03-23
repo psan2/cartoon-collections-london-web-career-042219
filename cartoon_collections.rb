61c592d12d613a286c91a0f8e8650e1e901433cf
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-23 12:56:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(menu)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  menu.map!{|x| cheese_types.include?(x) x : nil}
+  is_cheese << menu.map{|x| cheese_types.include?(x) x : nil}
 end
