66a5292dde7248e2228e3c52b70361f880633eb4
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-23 12:43:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls
-
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|word| word >= 4}  
 end
 
 def find_the_cheese# code an argument here
