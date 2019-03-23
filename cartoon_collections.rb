2c7d38469b8e31dabd4762f4b0e90c7c326eca89
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-23 12:40:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(roster)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect{|x| x + "!"}
+  planeteer_calls.collect{|x| x.upcase + "!"}
 end
 
 def long_planeteer_calls# code an argument here
